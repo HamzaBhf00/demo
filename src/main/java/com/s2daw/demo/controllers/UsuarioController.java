da0eaac45c747090988c42c359f0e8852a55c8e4
HamzaBhf00 <alumno.1248865@ies-azarquiel.es>
2023-03-01 20:14:44 +0100

    servicio REST y lista de usuarios usando fetch

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -4,6 +4,8 @@ import com.s2daw.demo.models.Usuario;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.Arrays;
 
@@ -20,7 +22,44 @@ public class UsuarioController {
         usuario.setTelefono("632656889");
         return usuario;
     }
+    @RequestMapping(value="usuario")
+    public List<Usuario> getUsuario() {
+        List<Usuario> usuarios = new ArrayList<>();
 
+        Usuario usuario = new Usuario();
+        usuario.setId(001L);
+        usuario.setNombre("Mario");
+        usuario.setApellido("Gonzales");
+        usuario.setEmail("mario@gmail.com");
+        usuario.setTelefono("6326598");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(002L);
+        usuario2.setNombre("Max");
+        usuario2.setApellido("Pablo");
+        usuario2.setEmail("max@gmail.com");
+        usuario2.setTelefono("6326598");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(003L);
+        usuario3.setNombre("Fran");
+        usuario3.setApellido("Fran");
+        usuario3.setEmail("fran@gmail.com");
+        usuario3.setTelefono("326598");
+
+        Usuario usuario4 = new Usuario();
+        usuario4.setId(004L);
+        usuario4.setNombre("Loco");
+        usuario4.setApellido("gris");
+        usuario4.setEmail("loco@gmail.com");
+        usuario4.setTelefono("326541");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        usuarios.add(usuario4);
+        return usuarios;
+    }
     @RequestMapping(value="usuario11") //esto es un URL
     public Usuario editar(){
         Usuario usuario = new Usuario();
@@ -50,5 +89,4 @@ public class UsuarioController {
         usuario.setTelefono("632656889");
         return usuario;
     }
-
 }
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,4 +1,24 @@
-// Call the dataTables jQuery plugin
 $(document).ready(function() {
+    cargaUsuario();
   $('#usuarios').DataTable();
 });
+
+async function cargaUsuario() {
+    const request = await fetch('usuario/2312', {
+    const request = await fetch('usuarios', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Context-Type': 'application/json'
+        }
+    });
+    const usuarios = await request.json();
+    console.log(usuarios);
+
+    let listadoHTML = '';
+    for (let usuario of usuarios) {
+        let usuarioHTML = '<tr><td>' + usuario.id + '</td><td>' + usuario.nombre + ' ' + usuario.apellido + '</td><td>' + usuario.email + '</td><td>'
+            + usuario.telefono + '</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"> <i class="fas fa-trash"></i></a></td></tr>'
+        listadoHTML += usuarioHTML;
+    }
+    document.querySelector('#usuarios tbody').outerHTML = listadoHTML;
