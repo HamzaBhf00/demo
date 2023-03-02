82d9b8c1d877b8104fd6bf91d884525e7940d1bd
HamzaBhf00 <alumno.1248865@ies-azarquiel.es>
2023-03-02 20:24:33 +0100

    Lombock

--- src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
+++ src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
@@ -4,13 +4,21 @@ import com.s2daw.demo.models.Usuario;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 @Transactional
 public class UsuarioDaoImp implements UsuarioDao{
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Override
     public List<Usuario> getUsuarios() {
-        return null;
+
+        String query = "FROM Usuario";
+        return entityManager.createQuery(query).getResultList();
+
     }
 }
