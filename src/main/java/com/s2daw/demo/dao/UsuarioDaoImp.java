3e4eef21e4f7f44a82b1c6f5fad2e7959965257e
HamzaBhf00 <alumno.1248865@ies-azarquiel.es>
2023-03-06 00:41:12 +0100

    Iniciar sesión y implementación de Argon2 (contraseñas Hash)

--- src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
+++ src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
@@ -1,6 +1,8 @@
 package com.s2daw.demo.dao;
 
 import com.s2daw.demo.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,5 +33,20 @@ public class UsuarioDaoImp implements UsuarioDao{
     @Override
     public void registrar(Usuario usuario) {
         entityManager.merge(usuario);
+    }
+
+    @Override
+    public boolean verificarCredenciales(Usuario usuario) {
+        String query = "FROM Usuario where email=:email";
+        List<Usuario> lista = entityManager.createQuery(query,Usuario.class)
+                .setParameter("email",usuario.getEmail())
+                .getResultList();
+
+       if (lista.isEmpty()) {
+            return false;
+        }
+        String passwordHashed = lista.get(0).getPassword();
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        return argon2.verify(passwordHashed, usuario.getPassword());
     }
 }
